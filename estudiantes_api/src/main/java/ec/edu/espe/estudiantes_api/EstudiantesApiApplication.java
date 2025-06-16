d90e4b16645316db3a625b2739dfdfaef2ec7f0c
RubenBenavidess <dario.benavides@hotmail.com>
2025-06-16 18:46:23 -0500

    Inicialicación de API

---
+++ estudiantes_api/src/main/java/ec/edu/espe/estudiantes_api/EstudiantesApiApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.estudiantes_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudiantesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudiantesApiApplication.class, args);
+	}
+
+}
---
+++ estudiantes_api/src/test/java/ec/edu/espe/estudiantes_api/EstudiantesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.estudiantes_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudiantesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
